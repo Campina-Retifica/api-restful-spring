660c562bd77c331a86facbbaaed17102ded77b1d
phlimadev <devphlima@gmail.com>
2025-04-12 08:31:27 -0300

    feat: criando serviço de validação de token

--- src/main/java/campina/retifica/api_rest/infrastructure/security/TokenService.java
+++ src/main/java/campina/retifica/api_rest/infrastructure/security/TokenService.java
@@ -4,6 +4,7 @@ import campina.retifica.api_rest.domain.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,20 @@ public class TokenService {
             return token;
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generating token.");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT
+                    .require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
         }
     }
 
