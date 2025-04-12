d71573ec2533f1a09c2db6a9f4664844562a1189
phlimadev <devphlima@gmail.com>
2025-04-12 07:47:48 -0300

    feat: adicionando método de buscar por username no repository

--- src/main/java/campina/retifica/api_rest/persistence/user/UserRepository.java
+++ src/main/java/campina/retifica/api_rest/persistence/user/UserRepository.java
@@ -1,11 +1,13 @@
-package campina.retifica.api_rest.persistence;
+package campina.retifica.api_rest.persistence.user;
 
 import campina.retifica.api_rest.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByUsername(String username);
 }
