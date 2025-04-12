d32a3b5e995f754d66117c507f37f6a0fa753587
phlimadev <devphlima@gmail.com>
2025-04-12 07:48:35 -0300

    feat: criando serviço de autenticação

---
+++ src/main/java/campina/retifica/api_rest/business/authentication/AuthenticationService.java
@@ -0,0 +1,19 @@
+package campina.retifica.api_rest.business.authentication;
+
+import campina.retifica.api_rest.persistence.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
