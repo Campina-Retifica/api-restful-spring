3660c45df85322bb90ac2939a2a6b6695a2fe178
phlimadev <devphlima@gmail.com>
2025-05-26 14:48:56 -0300

    feat: adding security filter to each of the requests made

--- src/main/java/br/edu/unifacisa/projeto_integrador/configuration/SecurityConfigurations.java
+++ src/main/java/br/edu/unifacisa/projeto_integrador/configuration/SecurityConfigurations.java
@@ -1,5 +1,7 @@
 package br.edu.unifacisa.projeto_integrador.configuration;
 
+import br.edu.unifacisa.projeto_integrador.security.SecurityFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfigurations {
+    private final SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -22,7 +28,8 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers(HttpMethod.POST, "/auth").permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().hasRole("USER"))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
