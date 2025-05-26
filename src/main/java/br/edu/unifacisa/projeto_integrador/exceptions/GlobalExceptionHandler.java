6e2322b98381cc03d50c5a58580d818ceee26560
phlimadev <devphlima@gmail.com>
2025-05-26 14:58:39 -0300

    feat: handling bad request errors

--- src/main/java/br/edu/unifacisa/projeto_integrador/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/edu/unifacisa/projeto_integrador/exceptions/GlobalExceptionHandler.java
@@ -3,13 +3,30 @@ package br.edu.unifacisa.projeto_integrador.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<Void> handleBadCredentialsException(BadCredentialsException e) {
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DataErrorValidation>> handleError400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(DataErrorValidation::new).toList());
+    }
+
+    public record DataErrorValidation(String field, String message) {
+        public DataErrorValidation(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
