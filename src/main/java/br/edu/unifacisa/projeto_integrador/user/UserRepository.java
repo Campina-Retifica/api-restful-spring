bddc86597cbcd9cf4dc5df3cd1c421a64ae2cf2d
phlimadev <devphlima@gmail.com>
2025-05-26 10:51:18 -0300

    feat: creating a method to search by username (will be used in the authentication service)

--- src/main/java/br/edu/unifacisa/projeto_integrador/user/UserRepository.java
+++ src/main/java/br/edu/unifacisa/projeto_integrador/user/UserRepository.java
@@ -1,8 +1,10 @@
 package br.edu.unifacisa.projeto_integrador.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    UserDetails findByUsername(String username);
 }
