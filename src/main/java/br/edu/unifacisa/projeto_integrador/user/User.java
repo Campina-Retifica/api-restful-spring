509b0cfc39c76bd6a80598a3f69c6d644a5cf351
phlimadev <devphlima@gmail.com>
2025-05-26 10:36:05 -0300

    feat: creating user entity

---
+++ src/main/java/br/edu/unifacisa/projeto_integrador/user/User.java
@@ -0,0 +1,33 @@
+package br.edu.unifacisa.projeto_integrador.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 50, unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", length = 120, nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
