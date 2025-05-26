54cc27bf6e118f2d60ed1ce7332f1717241d3e6e
phlimadev <devphlima@gmail.com>
2025-05-26 11:34:39 -0300

    feat: creating AuthenticationController

---
+++ src/main/java/br/edu/unifacisa/projeto_integrador/authentication/AuthenticationController.java
@@ -0,0 +1,24 @@
+package br.edu.unifacisa.projeto_integrador.authentication;
+
+import br.edu.unifacisa.projeto_integrador.user.UserLoginDTO;
+import br.edu.unifacisa.projeto_integrador.user.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> login(@RequestBody @Valid UserLoginDTO body) {
+        userService.login(body);
+        return ResponseEntity.ok().build();
+    }
+}
