640358a11ba73647555533a8a970f8a458507a43
bruno <brunosantosterze1314@gmail.com>
2025-06-03 15:38:36 -0300

    refactor: changing the data type of the status column

--- src/main/resources/db/migration/V4__create-table-services.sql
+++ src/main/resources/db/migration/V4__create-table-services.sql
@@ -1,16 +1,9 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'offered_service_enum') THEN
-        CREATE TYPE offered_service_enum AS ENUM ('PENDENTE', 'ATIVO', 'INATIVO');
-    END IF;
-END$$;
-
 CREATE TABLE tb_services (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
     service_value NUMERIC(10, 2) NOT NULL,
-    status offered_service_enum DEFAULT 'PENDENTE',
+    status VARCHAR(25) DEFAULT 'PENDENTE',
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
