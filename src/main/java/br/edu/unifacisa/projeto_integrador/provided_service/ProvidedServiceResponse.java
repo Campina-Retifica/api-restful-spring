caea63d461ffebad15cd153eaf41f8a36831fac3
phlimadev <devphlima@gmail.com>
2025-06-06 10:26:00 -0300

    feat: displaying the price of the service provided

--- src/main/java/br/edu/unifacisa/projeto_integrador/provided_service/ProvidedServiceResponse.java
+++ src/main/java/br/edu/unifacisa/projeto_integrador/provided_service/ProvidedServiceResponse.java
@@ -2,11 +2,13 @@ package br.edu.unifacisa.projeto_integrador.provided_service;
 
 import br.edu.unifacisa.projeto_integrador.offered_service.OfferedServiceEnum;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ProvidedServiceResponse(
         Long id,
         String serviceName,
+        BigDecimal price,
         String customerName,
         LocalDateTime startDate,
         LocalDateTime endDate,
@@ -14,6 +16,6 @@ public record ProvidedServiceResponse(
         Boolean paid
 ) {
     public ProvidedServiceResponse(ProvidedService service) {
-        this(service.getId(), service.getService().getName(), service.getCustomer().getFirstName(), service.getStartDate(), service.getEndDate(), service.getStatus(), service.getPaid());
+        this(service.getId(), service.getService().getName(), service.getService().getValue(), service.getCustomer().getFirstName(), service.getStartDate(), service.getEndDate(), service.getStatus(), service.getPaid());
     }
 }
